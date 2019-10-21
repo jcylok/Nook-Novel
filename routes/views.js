c6dc468eaeaabe6994275c21a12641a2b9848912
jacklok <jack22hk@gmail.com>
2019-10-21 14:36:39 -0700

    added auth

---
+++ routes/views.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+// ------------------------------- AUTH ---------------------------- //
+
+// GET Home
+router.get('/', (req, res) => {
+  res.sendFile('views/index.html', {
+    root: `${__dirname}/../`
+  });
+});
+
+// GET Signup
+router.get('/signup', (req, res) => {
+  res.sendFile('views/auth/signup.html', {
+    root: `${__dirname}/../`
+  });
+});
+
+
+// GET Login
+router.get('/login', (req, res) => {
+  res.sendFile('views/auth/login.html', {
+    root: `${__dirname}/../`
+  });
+});
+
+
+// ------------------------------- Profile ---------------------------- //
+
+// GET User Profile
+router.get('/profile/:userId', (req, res) => {
+  if (!req.session.currentUser) {
+    return res.redirect('/login');
+  }
+
+  res.sendFile('views/profile/show.html', {
+    root: `${__dirname}/../`
+  });
+});
+
+
+// ------------------------------- Book list ---------------------------- //
+
+// GET Book list
+router.get('/booklist', (req, res) => {
+    res.sendFile('views/booklist.html', {
+      root: `${__dirname}/../`
+    });
+  });
+  
+
+module.exports = router;
