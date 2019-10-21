34d60341c2c9c426a5e16b4038ae51cbc6dd1681
jaymark <jaykmark@gmail.com>
2019-10-21 15:56:14 -0700

    creates POST request to create session on login.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,30 @@
 const bcrypt = require('bcryptjs');
 const db = require('../models');
 
+const error500 = () => {
+  res.status(500).json({
+    status: 500,
+    error: [{ message: 'Something went wrong, please try again' }],
+  });
+};
+
 // POST Create User
 const createUser = (req, res) => {
   db.User.findOne( { email: req.body.email }, (err, foundUser) => {
-    if (err) return res.status(500).json({
-      status: 500,
-      error: [{ message: 'Something went wrong, please try again' }],
-    });
+    if (err) return error500();
 
     if (foundUser) return res.status(400).json({
       status: 400,
-      error: [{ message: 'Invalid request. Please try again."' }],
+      error: [{ message: 'Invalid request. Please try again.' }],
     });
 
     // Create Salt Rounds
     bcrypt.genSalt(10, (err, salt) => {
-      if (err) return res.status(500).json({
-        status: 500,
-        error: [{ message: 'Something went wrong. Please try again.' }],
-      });
+      if (err) return error500();
 
       bcrypt.hash(req.body.password, salt, (err, hash) => {
-        if (err) return res.status(500).json({
-          status: 500,
-          error: [{ message: 'Something went wrong. Please try again.'}],
-        });
+        if (err) return error500();
       
-
         // Can change sign up form
         const newUser = {
           firstName: req.body.firstName,
@@ -37,11 +34,7 @@ const createUser = (req, res) => {
         };
 
         db.User.create(newUser, (err, createdUser) => {
-          if (err) return res.status(500).json({
-            status: 500,
-            error: [{ message: 'Something went wrong. Please try again.'}],
-          });
-
+          if (err) return error500();
 
           res.status(201).json({
             status: 201,
@@ -51,3 +44,38 @@ const createUser = (req, res) => {
     });
   });
 };
+
+
+// POST Create Session
+const createSession = (req, res) => {
+  db.User.findOne({ email: req.body.email }, (err, foundUser) => {
+    if (err) return error500();
+
+    if (!foundUser) return res.status(400).json({
+      status: 400,
+      error: [{ message: 'Username or password is incorrect.'},]
+    });
+
+    bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+      if (err) return error500();
+
+      if (isMatch) {
+        req.session.currentUser = foundUser._id;
+        return res.status(201).json({
+          status: 201,
+          data: { id: foundUser._id },
+        });
+      } else {
+        return res.status(400).json({
+          status: 400,
+          error: [{ message: 'Username or password is incorrect.' }],
+        });
+      };
+    });
+  });
+};
+
+module.exports = {
+  createUser,
+  createSession,
+}
--- routes/api.js
+++ routes/api.js
@@ -5,21 +5,19 @@ const ctlr = require('../controllers');
 
 // ----------------------------- AUTH -------------------------- //
 
-router.post('/signup', ctrl.auth.createUser);
-router.post('/login', ctrl.auth.createSession);
-router.delete('/logout', ctrl.auth.deleteSession);
-router.get('/verify', ctrl.auth.verifyAuth);
+router.post('/signup', ctlr.auth.createUser);
+router.post('/login', ctlr.auth.createSession);
+// router.delete('/logout', ctlr.auth.deleteSession);
+// router.get('/verify', ctlr.auth.verifyAuth);
 
 
 // ----------------------------- PROFILE -------------------------- //
 
-router.get('/profile/:userId', ctrl.auth.showProfile);
+// router.get('/profile/:userId', ctlr.auth.showProfile);
 
 
 // ----------------------------- BOOK LIST -------------------------- //
-router.get('/booklist', ctrl.auth.showProfile);
-
-
+// router.get('/booklist', ctlr.auth.showProfile);
 
 
 module.exports = router;
