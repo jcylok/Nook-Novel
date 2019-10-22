0057eed06710a3ab8c14a88eb61607d349fb14b4
jacklok <jack22hk@gmail.com>
2019-10-22 10:41:25 -0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    users: require('./users'),
-    views: require('./views'),
-=======
   users: require('./users.js'),
   books: require('./books.js'),
->>>>>>> 8d6b74309e8bf25de46d7eb362d12efb8abe11a2
 }
